ab69ca51ad322960e1b1e6071b1abdf338d37829
Ken Ochieng <kenochiengstudy@gmail.com>
2021-11-16 09:48:27 +0300

    First calculator js functions commit

---
+++ calc.js
@@ -0,0 +1,39 @@
+const operators = ['+','-','/','*'];
+const operation = "";
+
+
+
+
+const addNums = (...nums) =>{
+    let result = nums.reduce((num1,num2)=>num1+num2);
+    return result;
+
+};
+
+const subTract = (a,b)=> {
+    let result = (a-b);
+    return result;
+
+};
+
+
+const multiply = (a,b)=> {
+    let result = (a*b);
+    return result;
+    
+ };
+
+
+ const divide = (a,b)=>{
+    let result = (a/b);
+    return result;
+ };
+
+
+ function operate(firstNum,secondNum,operation){
+    
+
+ };
+
+
+
